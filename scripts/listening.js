c939c1e9a45d10b0713382666fda43e5275a9c5e
Daana Banerjee <dbanerjee889@gmail.com>
2017-02-20 21:04:12 -0800

    added new commands

--- scripts/listening.js
+++ scripts/listening.js
@@ -3,15 +3,15 @@
  module.exports = function(bot) {
   bot.hear(/Hello!/, function(res) {
    return res.send("Hi there!");
- });
- };
-
-/*
-bot.respond(/What's your favorite food?/, function(res) {
-   return res.send("I'm a robot--I don't eat food!");
   });
 
-bot.respond(/Hi Hubot! My name is (.*)/i, function(msg) {
+/*
+  bot.respond(/What's your favorite food?/, function(res) {
+   return res.send("I'm a robot--I don't eat food!");
+  });
+*/
+
+  bot.respond(/Hi Hubot! My name is (.*)/i, function(msg) {
    var name;
    name = msg.match[1];
    if (name == "Hubot"){
@@ -19,5 +19,6 @@ bot.respond(/Hi Hubot! My name is (.*)/i, function(msg) {
    } else {
      return msg.reply("Nice to meet you, " + name + "!");
    }
- });
- /*
+  });
+
+};
